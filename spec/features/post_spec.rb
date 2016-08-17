84431bb7e511cf8d33c9c97e06bdf6f7661fe27b
jaspercurry <jaspercurry@gmail.com>
2016-08-17 16:54:32 -0400

    made forms

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -21,4 +21,19 @@ describe 'new post' do
 
     expect(page).to have_content("My post title")
   end
+
+  it 'shows a new form that submits content and redirects to new page and prints out params' do
+  visit new_post_path
+
+  # The spec currently calls these fields "title"
+  # and "description" — you can change that for now,
+  # then change it back later after we refactor (this is
+  # a pretty common workflow — it's best to get used to it!)
+  fill_in 'title', with: "My post title"
+  fill_in 'description', with: "My post description"
+
+  click_on "Submit Post"
+
+  expect(page).to have_content("My post title")
+end
 end
